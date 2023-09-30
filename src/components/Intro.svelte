b795ef4d4a2847b3df10a18684c7d36757a5af9c
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2023-09-30 10:23:54 +0800

    added fonts, setup tailwind svelte

---
+++ src/components/Intro.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-full h-[90vh] text-white flex justify-center items-center">
+    <div class="w-4/5 flex flex-row">
+        <div class="text-4xl flex flex-col">
+            <p>
+                Nice to meet you, I am
+                <br>
+                Ryan
+                <br>
+                I like to make cool stuff
+                <br>
+                as a programmer
+            </p>
+        </div>
+    </div>
+</div>
