b7ef1bdc4b528e1c4d1a463532aec74b1cb5ad3c
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-08-08 12:15:03 +1000

    navbar working

--- src/components/ui/HoverNavigation.tsx
+++ src/components/ui/HoverNavigation.tsx
@@ -40,44 +40,79 @@ export const HoverNavigation = ({
     });
 
     return (
-        <motion.div
-            animate={{
-                width: visible ? "auto" : "0px",
-            }}
-            className={cn(
-                "flex flex-row items-center justify-center overflow-hidden",
-                className,
-            )}
-        >
-            {items.map((item, idx) => (
+        <>
+            <motion.div
+                animate={{
+                    width: visible ? "0px" : "auto",
+                    opacity: visible ? "0%" : "100%",
+                }}
+                transition={{
+                    ease: "easeInOut",
+                }}
+                className={cn(
+                    "flex w-0 flex-row items-center justify-center whitespace-nowrap",
+                    className,
+                )}
+            >
                 <a
-                    href={item?.link}
-                    key={item?.link}
-                    className="group relative block h-full px-5 py-2"
-                    onMouseEnter={() => setHoveredIndex(idx)}
-                    onMouseLeave={() => setHoveredIndex(null)}
+                    href="/contact"
+                    className={`overflow-hidden whitespace-nowrap transition-colors duration-150 hover:text-green-500 ${!visible ? "" : "inline-block"}`}
                 >
-                    <AnimatePresence>
-                        {hoveredIndex === idx && (
-                            <motion.span
-                                className="absolute inset-0 block h-full w-full rounded-3xl bg-neutral-200"
-                                layoutId="hoverBackground"
-                                initial={{ opacity: 0 }}
-                                animate={{
-                                    opacity: 1,
-                                    transition: { duration: 0.15 },
-                                }}
-                                exit={{
-                                    opacity: 0,
-                                    transition: { duration: 0.15, delay: 0.2 },
-                                }}
-                            />
-                        )}
-                    </AnimatePresence>
-                    <NavBarLink>{item.label}</NavBarLink>
+                    Available for work
                 </a>
-            ))}
-        </motion.div>
+                <div
+                    className={`relative z-50 ml-3 w-5 ${visible ? "hidden" : "inline-block"}`}
+                >
+                    <div className="absolute top-1/2 left-1/2 aspect-square w-[6px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-green-500"></div>
+                    <div className="animate-glow absolute top-1/2 left-1/2 aspect-square w-[6px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-green-500"></div>
+                </div>
+            </motion.div>
+            <motion.div
+                animate={{
+                    width: visible ? "auto" : "0px",
+                    opacity: visible ? "100%" : "0%",
+                }}
+                transition={{
+                    ease: "easeInOut",
+                }}
+                className={cn(
+                    "flex flex-row items-center justify-center overflow-hidden",
+                    className,
+                )}
+            >
+                {items.map((item, idx) => (
+                    <a
+                        href={item?.link}
+                        key={item?.link}
+                        className="group relative block h-full px-5 py-2"
+                        onMouseEnter={() => setHoveredIndex(idx)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        <AnimatePresence>
+                            {hoveredIndex === idx && (
+                                <motion.span
+                                    className="absolute inset-0 block h-full w-full rounded-3xl bg-neutral-200"
+                                    layoutId="hoverBackground"
+                                    initial={{ opacity: 0 }}
+                                    animate={{
+                                        opacity: 1,
+                                        transition: { duration: 0.15 },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        transition: {
+                                            duration: 0.15,
+                                            delay: 0.2,
+                                        },
+                                    }}
+                                />
+                            )}
+                        </AnimatePresence>
+                        <NavBarLink>{item.label}</NavBarLink>
+                    </a>
+                ))}
+            </motion.div>
+        </>
     );
 };
 
