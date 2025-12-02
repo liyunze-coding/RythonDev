ba3f70141106658f174932c0aede5e0d5b613292
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-12-02 12:56:09 +0800

    updated component

--- src/components/experience/ExperienceComponent.tsx
+++ src/components/experience/ExperienceComponent.tsx
@@ -4,7 +4,7 @@ export default function Experience() {
 	return (
 		<Tabs
 			defaultValue="experience"
-			className="view-fadeUp w-[90%] md:w-[600px] lg:w-1/2"
+			className="w-[90%] md:w-[600px] lg:w-1/2"
 		>
 			<TabsList className="w-full">
 				<TabsTrigger value="experience">Experience</TabsTrigger>
